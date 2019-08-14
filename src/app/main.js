84dae6552702d0e09380034bdb36965dc52a2c55
Roger Winright <rogerwinright@gmail.com>
2019-08-14 08:58:32 -0400

    experimenting with platforms

--- src/app/main.js
+++ src/app/main.js
@@ -46,20 +46,32 @@ const Right_Wall = Sprite({
   color: 'brown'
 })
 
+const Platform = Sprite({
+  x: canvas.width/2,
+  y: 220,
+  height: 5,
+  width: 40,
+  color: 'brown'
+})
+
 console.log(canvas.height)
 let loop = GameLoop({  // create the main game loop
   update: function () { // game logic goes here
     Ground.update();
     Left_Wall.update();
     Right_Wall.update();
+    Platform.update();
     Player_1.update();
     timer++;
     //Another variable that we can change the value of.
     currentTime = timer;
 
+
+    //Todo: DEFINITELY going to have to clean this and the sprites up.
     let Player_Ground_Collide = Collide(Player_1, Ground);
     let Player_Left_Collide = Collide(Player_1, Left_Wall);
     let Player_Right_Collide = Collide(Player_1, Right_Wall);
+    let Platform_Collide = Collide(Player_1, Platform)
     
     Jump(keyPressed('w'), Player_1);
     Movement({left: keyPressed('a'), right: keyPressed('d')}, Player_1);
@@ -67,7 +79,7 @@ let loop = GameLoop({  // create the main game loop
     if (Player_Left_Collide === "l" || Player_Right_Collide === "r") {
       Player_1.dx = 0;
       Player_1.jumping = false;
-    } else if (Player_Ground_Collide === "b") {
+    } else if (Player_Ground_Collide === "b" || Platform_Collide === "b") {
       Player_1.grounded = true;
       Player_1.jumping = false;
       //Set the timer back to 0 if the bottom is touching.
@@ -85,6 +97,7 @@ let loop = GameLoop({  // create the main game loop
   },
   render: function () { // render the game state
     Player_1.render();
+    Platform.render();
     Ground.render();
     Left_Wall.render();
     Right_Wall.render();
