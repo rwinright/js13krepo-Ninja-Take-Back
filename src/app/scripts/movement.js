8ed23d9612c97037c176729878b8c5b1a78b1da7
Roger Winright <rogerwinright@gmail.com>
2019-08-25 19:55:18 -0400

    created sprite assets

--- src/app/scripts/movement.js
+++ src/app/scripts/movement.js
@@ -1,10 +1,11 @@
 export const Jump = (jumpPressed, player) => {
-
+  // player.scale(-1, 1)
+  // player.context.canvas;
   if (jumpPressed) {
     if (!player.jumping && player.grounded) {
       player.jumping = true;
       player.grounded = false;
-
+      
       if (player.dy > -10) {
         player.dy -= 3.2;
       }
@@ -12,22 +13,31 @@ export const Jump = (jumpPressed, player) => {
     }
   }
   // else if (!jumpPressed && player.jumping) {
-  //   //player.jumping = false;
-  //   //player.dy += .3;
-  // }
-}
-
-
-
-export const Movement = (keyPress, player) => {
-  if (keyPress.right) {
+    //   //player.jumping = false;
+    //   //player.dy += .3;
+    // }
+  }
+  
+  
+  
+  export const Movement = (keyPress, player) => {
+    if (keyPress.right) {
     // right arrow
+    player.facing = 'right'
     player.dx = player.speed;
+    player.playAnimation('walk_right')
   } else if (keyPress.left) {
     // left arrow
     player.dx = -player.speed;
+    player.facing = 'left'
+    player.playAnimation('walk_left')
   } else {
     player.dx = 0;
+    if(player.facing === 'left'){
+      player.playAnimation('idle_left');
+    } else {
+      player.playAnimation('idle_right');
+    }
   }
 }
 
