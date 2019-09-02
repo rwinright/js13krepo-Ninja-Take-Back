f63858c246beb21345a994dafbcb20bb712e86f1
Tommy Waalkes <Tommy@grandcircus.co>
2019-09-01 20:14:55 -0400

    Fixed up Merge issues

--- src/app/main.js
+++ src/app/main.js
@@ -18,6 +18,8 @@ p1_spriteSheet.onload = function () {
   let gravity = .09;
 
   let platforms = [];
+  let gui = [];
+  let objects = [];
 
   //P1 Spritesheet function
 
@@ -46,32 +48,32 @@ p1_spriteSheet.onload = function () {
     }
   });
 
-    //P1 Spritesheet function
+  //P1 Spritesheet function
 
-    let P2_SpriteSheet = SpriteSheet({
-      image: p1_spriteSheet,
-      frameWidth: 72,
-      frameHeight: 72,
-      animations: {
-        // create a named animation: walk
-        walk_right: {
-          frames: [4, 5, 6, 7],  // frames 0 through 9
-          frameRate: 10
-        },
-        walk_left: {
-          frames: [3, 2, 1, 0],
-          frameRate: 10
-        },
-        idle_right: {
-          frames: [4],
-          frameRate: 1
-        },
-        idle_left: {
-          frames: [3],
-          frameRate: 1
-        }
+  let P2_SpriteSheet = SpriteSheet({
+    image: p1_spriteSheet,
+    frameWidth: 72,
+    frameHeight: 72,
+    animations: {
+      // create a named animation: walk
+      walk_right: {
+        frames: [4, 5, 6, 7],  // frames 0 through 9
+        frameRate: 10
+      },
+      walk_left: {
+        frames: [3, 2, 1, 0],
+        frameRate: 10
+      },
+      idle_right: {
+        frames: [4],
+        frameRate: 1
+      },
+      idle_left: {
+        frames: [3],
+        frameRate: 1
       }
-    });
+    }
+  });
 
   //Ground
 
@@ -117,7 +119,8 @@ p1_spriteSheet.onload = function () {
     height: 190,
     width: 50,
     color: 'black',
-    slowPlayer: false
+    slowPlayer: false,
+    isClimbable: true
   })
 
   const End = Sprite({//Dynamically adjusts to be next to right wall
@@ -152,8 +155,6 @@ p1_spriteSheet.onload = function () {
     x: (Spawn.width + Left_Wall.width) - 20, // starting x,y position of the sprite based on spawn
     y: Spawn.y - 40,
     animations: P1_SpriteSheet.animations,
-    // color: 'red',
-    // anchor: {x: 0.5, y: 0.5},
     width: 20,
     height: 20,
     facing: 'right', // Check player facing
@@ -161,7 +162,9 @@ p1_spriteSheet.onload = function () {
     dy: 0,
     jumping: true,
     grounded: false,
+    climbing: false,
     speed: 3,
+    speed_base: 3,
     max_fall_speed: 10
   });
 
@@ -178,11 +181,78 @@ p1_spriteSheet.onload = function () {
     jumping: true,
     grounded: false,
     speed: 3,
+    speed_base: 3,
     max_fall_speed: 10
   });
 
-  platforms.push(Ground, Left_Wall, Right_Wall, Top_Wall, Spawn, End, Platform, Ground_Slow);
+  const ItemBoxBottom = Sprite({
+    x: 0,
+    y: 50,
+    height: 10,
+    width: canvas.width,
+    color: 'black'
+  })
 
+  const ItemBoxTop = Sprite({
+    x: 0,
+    y: 0,
+    width: canvas.width,
+    height: 10,
+    color: 'black'
+  })
+
+  const ItemBoxLeft = Sprite({
+    x: 0,
+    y: 0,
+    height: 50,
+    width: 15,
+    color: 'black'
+  })
+
+  const ItemBoxRight = Sprite({
+    height: 50,
+    width: 15,
+    x: canvas.width - 15,
+    y: 0,
+    color: 'black'
+  })
+
+  const Divider = Sprite({
+    height: 50,
+    width: 20,
+    x: canvas.width / 2,
+    y: 0,
+    color: 'black'
+  })
+
+  const rocket = Sprite({
+    x: 140,
+    y: 200,
+    height: 10,
+    width: 100
+  })
+
+  const portal = Sprite({
+    x: 200,
+    y: 200,
+    height: 20,
+    width: 10,
+    color: 'purple'
+  })
+
+  const coffee = Sprite({
+    x: 200,
+    y: 190,
+    width: 10,
+    height: 15,
+    color: 'brown'
+  })
+
+  platforms.push(Ground, Ground_Slow, Left_Wall, Right_Wall, Top_Wall, Spawn, End, Platform)
+
+  gui.push(ItemBoxBottom, ItemBoxTop, ItemBoxLeft, ItemBoxRight, Divider);
+
+  objects.push(rocket, portal, coffee)
   //Text stuff!
   context.fillStyle = 'black'
   context.font = '10px Courier New'
@@ -231,6 +301,12 @@ p1_spriteSheet.onload = function () {
       for (let i = 0; i < platforms.length; i++) {
         platforms[i].render();
       }
+      for (let i = 0; i < objects.length; i++) {
+        objects[i].render();
+      }
+      for (let i = 0; i < gui.length; i++) {
+        gui[i].render();
+      }
 
 
       // Good-ass mouse tool
@@ -240,36 +316,73 @@ p1_spriteSheet.onload = function () {
     }
   });
 
+
   function applyGravity(player) {
-    if (player.grounded && !player.jumping) {
-      player.dy = 0;
-    } else {
-      if (player.dy < player.max_fall_speed) {
-        player.dy += gravity;
-      }
+    if (player.dy < player.max_fall_speed && !player.climbing && player.dy < 10) {
+      player.dy += gravity;
     }
   }
 
   function applyCollision(player) {
 
     for (let i = 0; i < platforms.length; i++) {
+      let plat = platforms[i];
       platforms[i].update();
 
       let platformCol = Collide(player, platforms[i]);
 
       if (platformCol === "l" || platformCol === "r") {
         player.dx = 0;
+        if (plat.isClimbable) {
+          player.dy = -10;
+
+        }
       }
       else if (platformCol === "b") {
         player.dy = 0;
+        player.ddy = 0;
         player.jumping = false;
         player.grounded = true;
-        // added a slow property to platforms as I believe we 'may' be using this property more.
-        platforms[i].slowPlayer ? player.speed = 1.3 : player.speed = 3
+        player.climbing = false;
+        player.speed = player.speed_base;
       }
       else if (platformCol === "t") {
         player.dy = 0;
+        player.climbing = false;
       }
+      let slowCol = Collide(player, Ground_Slow);
+
+      if (slowCol === "b") {
+        player.speed = player.speed_base / 2;
+      }
+
+
+
+      //respawn if out of bounds
+      if (player.x > canvas.width || player.y > canvas.height || player.x < 0 || player.y < 0) {
+        player.x = 30;
+        player.y = 170;
+        player.dx = 0;
+        player.ddy = 0;
+      }
+
+    }
+    if (player.collidesWith(rocket)) {
+      player.ddy = 0;
+      player.dy = -9;
+    }
+
+    if (player.collidesWith(portal)) {
+      player.x = 30;
+      player.y = 185;
+    }
+
+    if (player.collidesWith(coffee)) {
+      objects = objects.filter(function (c) {
+        return c != coffee;
+      })
+
+      player.speed_base = 5;
 
     }
   }
